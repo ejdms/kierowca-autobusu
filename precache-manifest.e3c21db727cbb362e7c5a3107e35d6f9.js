581934d0109eeaa8b425840c2296ce895e529361
ejdms <52212392+ejdms@users.noreply.github.com>
2020-01-22 21:03:21 +0100

    Updates

---
+++ precache-manifest.e3c21db727cbb362e7c5a3107e35d6f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a7e6146f3254878c97274b5047a5d7",
+    "url": "/kierowca-autobusu/index.html"
+  },
+  {
+    "revision": "d298e4c869100d8ef755",
+    "url": "/kierowca-autobusu/static/css/main.a1342bfa.chunk.css"
+  },
+  {
+    "revision": "a55ff93e4221a7508f09",
+    "url": "/kierowca-autobusu/static/js/2.d3d1f8ce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kierowca-autobusu/static/js/2.d3d1f8ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d298e4c869100d8ef755",
+    "url": "/kierowca-autobusu/static/js/main.fba9f7d8.chunk.js"
+  },
+  {
+    "revision": "ef1e73bb02158baa60ac",
+    "url": "/kierowca-autobusu/static/js/runtime-main.452d2f0e.js"
+  }
+]);
